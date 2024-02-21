b590b4b99849573a9a5ba2845f7e992331935e00
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-02-21 16:38:02 +1100

    do question 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR";
+    };
 
+    let outputString = "";
+    let n = 1;
+    while (n <= num) {
+        outputString += string;
+        n++;
+    }
+
+    return outputString
 };
 
 // Do not edit below this line
