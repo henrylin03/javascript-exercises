7fda69f1408430bf43a814ac1f4b92a71d353cb6
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-02-21 17:31:48 +1100

    feat: return error when there are non-number (integer) arguments
    just added to the list of conditions, separated by OR ||

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
 const sumAll = function (num1, num2) {
-    if (num1 < 0 || num2 < 0) { return "ERROR" };
+    if (num1 < 0 || num2 < 0 ||
+        !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR"
+    };
 
     const start = Math.min(...arguments);
     const end = Math.max(...arguments);
