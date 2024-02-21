5009367be9e7d13d33490cdcd7842e41176ce08a
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-02-21 17:11:40 +1100

    use while loop to sum every number from start number to end number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+    let sum = 0;
 
+    let n = startNum;
+    while (n <= endNum) {
+        sum += n;
+        n++;
+    };
+
+    return sum;
 };
 
 // Do not edit below this line
